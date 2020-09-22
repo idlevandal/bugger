9e9b20d27f76a654af129573be7ee4409f41283c
David Allen <davetvmusic@hotmail.com>
2020-09-22 17:03:28 +1000

    add helmet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 require('dotenv').config();
 
 const connectDB = require('./config/db');
@@ -10,6 +11,7 @@ const userRoute = require('./routes/user.route');
 
 const PORT = process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
